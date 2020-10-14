d489fb998dad029c7187dbcf383a303b5fd8e45e
Rene Copaga <rene.copaga@gmail.com>
2020-10-14 11:13:27 -0400

    Preparing1

--- Startup.cs
+++ Startup.cs
@@ -18,29 +18,16 @@ namespace Platform
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<MessageOptions>(options => {
-                options.CityName = "Albany";
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseMiddleware<LocationMiddleware>();
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            app.UseDeveloperExceptionPage();
+            app.UseMiddleware<Population>();
+            app.UseMiddleware<Capital>();
+            app.Use(async (context, next) => {
+                await context.Response.WriteAsync("Terminal Middleware Reached");
             });
         }
     }
