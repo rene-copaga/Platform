7eb82bdafc68eff605a85930258f2eda2cd67396
Rene Copaga <rene.copaga@gmail.com>
2020-10-14 18:50:18 -0400

    Using a Service in a Middleware Class

--- WeatherMiddleware.cs
+++ WeatherMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Platform.Services;
 using System.Threading.Tasks;
 
 namespace Platform
@@ -6,16 +7,19 @@ namespace Platform
     public class WeatherMiddleware
     {
         private RequestDelegate next;
-        public WeatherMiddleware(RequestDelegate nextDelegate)
+        private IResponseFormatter formatter;
+        public WeatherMiddleware(RequestDelegate nextDelegate,
+                IResponseFormatter respFormatter)
         {
             next = nextDelegate;
+            formatter = respFormatter;
         }
         public async Task Invoke(HttpContext context)
         {
             if (context.Request.Path == "/middleware/class")
             {
-                await context.Response
-                .WriteAsync("Middleware Class: It is raining in London");
+                await formatter.Format(context, 
+                    "Middleware Class: It is raining in London");
             }
             else
             {
