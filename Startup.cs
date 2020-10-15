90b69aa6cc9416f0eb1d1aa2325942aed21fec1c
Rene Copaga <rene.copaga@gmail.com>
2020-10-15 14:19:42 -0400

    Accessing Configuration Settings

--- Startup.cs
+++ Startup.cs
@@ -16,10 +16,18 @@ namespace Platform
         public void ConfigureServices(IServiceCollection services)
         {
         }
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
+                IConfiguration config)
         {
             app.UseDeveloperExceptionPage();
             app.UseRouting();
+
+            app.Use(async (context, next) => {
+                string defaultDebug = config["Logging:LogLevel:Default"];
+                await context.Response
+                    .WriteAsync($"The config setting is: {defaultDebug}");
+            });
+
             app.UseEndpoints(endpoints => {
                 endpoints.MapGet("/", async context => {
                     await context.Response.WriteAsync("Hello World!");
