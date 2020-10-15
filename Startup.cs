9d915542b63a5945acf254d516b493a9301a1e5f
Rene Copaga <rene.copaga@gmail.com>
2020-10-15 18:40:18 -0400

    Configuring the Session Service and Middleware

--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,13 @@ namespace Platform
             services.Configure<CookiePolicyOptions>(opts => {
                 opts.CheckConsentNeeded = context => true;
             });
+
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options => {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.IsEssential = true;
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
@@ -22,37 +29,10 @@ namespace Platform
             app.UseDeveloperExceptionPage();
             app.UseCookiePolicy();
             app.UseMiddleware<ConsentMiddleware>();
+            app.UseSession();
             app.UseRouting();
 
             app.UseEndpoints(endpoints => {
-
-                endpoints.MapGet("/cookie", async context => {
-                    int counter1 =
-                        int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
-                    context.Response.Cookies.Append("counter1", counter1.ToString(),
-                        new CookieOptions
-                        {
-                            MaxAge = TimeSpan.FromMinutes(30),
-                            IsEssential = true
-                        });
-                    int counter2 =
-                        int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
-                    context.Response.Cookies.Append("counter2", counter1.ToString(),
-                        new CookieOptions
-                        {
-                            MaxAge = TimeSpan.FromMinutes(30)
-                        });
-                    await context.Response
-                        .WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
-                });
-
-                endpoints.MapGet("clear", context => {
-                    context.Response.Cookies.Delete("counter1");
-                    context.Response.Cookies.Delete("counter2");
-                    context.Response.Redirect("/");
-                    return Task.CompletedTask;
-                });
-
                 endpoints.MapFallback(async context =>
                     await context.Response.WriteAsync("Hello World!"));
             });
