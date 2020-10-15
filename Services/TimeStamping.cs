7188c13330241696035e86167c7219cb0c5a4354
Rene Copaga <rene.copaga@gmail.com>
2020-10-14 21:30:26 -0400

    Creating Dependency Chains

---
+++ Services/TimeStamping.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Platform.Services
+{
+    public interface ITimeStamper
+    {
+        string TimeStamp { get; }
+    }
+    public class DefaultTimeStamper : ITimeStamper
+    {
+        public string TimeStamp
+        {
+            get => DateTime.Now.ToShortTimeString();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -13,7 +13,8 @@ namespace Platform
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IResponseFormatter, GuidService> ();
+            services.AddScoped<IResponseFormatter, TimeResponseFormatter>();
+            services.AddScoped<ITimeStamper, DefaultTimeStamper>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
