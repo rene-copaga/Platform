773d7220cd1578c04a0d92565a9c7169e9a87ea5
Rene Copaga <rene.copaga@gmail.com>
2020-10-15 14:01:27 -0400

    Preparing

--- Startup.cs
+++ Startup.cs
@@ -13,44 +13,16 @@ namespace Platform
 {
     public class Startup
     {
-        public Startup(IConfiguration config)
-        {
-            Configuration = config;
-        }
-
-        private IConfiguration Configuration;
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton(typeof(ICollection<>), typeof(List<>));
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseDeveloperExceptionPage();
             app.UseRouting();
             app.UseEndpoints(endpoints => {
-                endpoints.MapGet("/string", async context => {
-                    ICollection<string> collection
-                        = context.RequestServices.GetService<ICollection<string>>();
-                    collection.Add($"Request: { DateTime.Now.ToLongTimeString() }");
-                    foreach (string str in collection)
-                    {
-                        await context.Response.WriteAsync($"String: {str}\n");
-                    }
-                });
-
-                endpoints.MapGet("/int", async context => {
-                    ICollection<int> collection
-                        = context.RequestServices.GetService<ICollection<int>>();
-                    collection.Add(collection.Count() + 1);
-                    foreach (int val in collection)
-                    {
-                        await context.Response.WriteAsync($"Int: {val}\n");
-                    }
+                endpoints.MapGet("/", async context => {
+                    await context.Response.WriteAsync("Hello World!");
                 });
             });
         }
