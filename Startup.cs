cd9ed14829c93f98d1f1e8abd93724d484331c29
Rene Copaga <rene.copaga@gmail.com>
2020-10-15 17:27:44 -0400

    Enabling Cookie Consent Checking

--- Startup.cs
+++ Startup.cs
@@ -12,11 +12,15 @@ namespace Platform
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(opts => {
+                opts.CheckConsentNeeded = context => true;
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
             app.UseDeveloperExceptionPage();
+            app.UseCookiePolicy();
             app.UseRouting();
 
             app.UseEndpoints(endpoints => {
@@ -27,7 +31,8 @@ namespace Platform
                     context.Response.Cookies.Append("counter1", counter1.ToString(),
                         new CookieOptions
                         {
-                            MaxAge = TimeSpan.FromMinutes(30)
+                            MaxAge = TimeSpan.FromMinutes(30),
+                            IsEssential = true
                         });
                     int counter2 =
                         int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
