39a6fad3dcbda88e2ca897480c5b33219965c9e4
Rene Copaga <rene.copaga@gmail.com>
2020-10-15 17:22:35 -0400

    Using Cookies

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 
 namespace Platform
 {
@@ -18,8 +20,35 @@ namespace Platform
             app.UseRouting();
 
             app.UseEndpoints(endpoints => {
+
+                endpoints.MapGet("/cookie", async context => {
+                    int counter1 =
+                        int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
+                    context.Response.Cookies.Append("counter1", counter1.ToString(),
+                        new CookieOptions
+                        {
+                            MaxAge = TimeSpan.FromMinutes(30)
+                        });
+                    int counter2 =
+                        int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
+                    context.Response.Cookies.Append("counter2", counter1.ToString(),
+                        new CookieOptions
+                        {
+                            MaxAge = TimeSpan.FromMinutes(30)
+                        });
+                    await context.Response
+                        .WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
+                });
+
+                endpoints.MapGet("clear", context => {
+                    context.Response.Cookies.Delete("counter1");
+                    context.Response.Cookies.Delete("counter2");
+                    context.Response.Redirect("/");
+                    return Task.CompletedTask;
+                });
+
                 endpoints.MapFallback(async context =>
-                await context.Response.WriteAsync("Hello World!"));
+                    await context.Response.WriteAsync("Hello World!"));
             });
         }
     }
