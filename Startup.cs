1b73dacc52bae155c53449cd9755e28d664ed671
Rene Copaga <rene.copaga@gmail.com>
2020-10-15 19:13:52 -0400

    Handling Exceptions and Errors

--- Startup.cs
+++ Startup.cs
@@ -32,8 +32,11 @@ namespace Platform
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
-            app.UseDeveloperExceptionPage();
-            if (env.IsProduction())
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
             {
                 app.UseHsts();
             }
@@ -41,27 +44,9 @@ namespace Platform
             app.UseCookiePolicy();
             app.UseMiddleware<ConsentMiddleware>();
             app.UseSession();
-            app.UseRouting();
 
-            app.Use(async (context, next) => {
-                await context.Response.WriteAsync($"HTTPS Request: {context.Request.IsHttps} \n");
-                await next();
-            });
-
-            app.UseEndpoints(endpoints => {
-
-                endpoints.MapGet("/cookie", async context => {
-                    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
-                    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
-                    context.Session.SetInt32("counter1", counter1);
-                    context.Session.SetInt32("counter2", counter2);
-                    await context.Session.CommitAsync();
-                    await context.Response
-                    .WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
-                });
-
-                endpoints.MapFallback(async context =>
-                    await context.Response.WriteAsync("Hello World!"));
+            app.Run(context => {
+                throw new Exception("Something has gone wrong");
             });
         }
     }
